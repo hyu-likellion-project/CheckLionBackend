4e21833698315fdafc52c0ba3e4acab86693eed3
joy <joyuuuu1@gmail.com>
2021-05-04 20:45:37 +0900

    [feat#6] DB모델 수정 & Team CRUD

--- checklionproj/checklion/admin.py
+++ checklionproj/checklion/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Team,Student,Status,Week,TeamPoint
+from .models import Status,Week,TeamPoint
 
 # Register your models here.
-admin.site.register(Team)
-admin.site.register(Student)
+
 admin.site.register(Status)
 admin.site.register(Week)
 admin.site.register(TeamPoint)
--- checklionproj/checklionproj/settings.py
+++ checklionproj/checklionproj/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'checklion.apps.ChecklionConfig',
+    'people.apps.PeopleConfig',
 ]
 
 MIDDLEWARE = [
