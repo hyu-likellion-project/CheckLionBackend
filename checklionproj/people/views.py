c46fd1da60d90d8090e1087c1e579a90710d58aa
sgwon96 <zxcvb5434@likelion.org>
2021-05-07 01:28:32 +0900

    [feat#13] teamId로 팀에속한 유저찾기

--- checklionproj/people/views.py
+++ checklionproj/people/views.py
@@ -10,3 +10,11 @@ class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
+    def get_queryset(self):
+        queryset = Student.objects.all()
+        teamid = self.request.query_params.get("teamid", None)
+        if teamid is not None:
+            queryset = queryset.filter(team_id=teamid)
+        return queryset
+
+
