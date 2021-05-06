a8135844ae9713a82426b51a7865cb8ea22c5b9b
sgwon96 <zxcvb5434@likelion.org>
2021-05-07 02:07:22 +0900

    [feat#14] studentid, week 로 해당 status찾기

--- checklionproj/checklion/views.py
+++ checklionproj/checklion/views.py
@@ -6,6 +6,15 @@ class StatusViewSet(viewsets.ModelViewSet):
     queryset = Status.objects.all()
     serializer_class = StatusSerializer
 
+    def get_queryset(self):
+        queryset = Status.objects.all()
+        studentid = self.request.query_params.get("studentid", None)
+        week = self.request.query_params.get("week", None)
+        if studentid is not None and week is not None:
+            queryset = queryset.filter(student=studentid)
+            queryset = queryset.filter(week=week)
+        return queryset
+
 class TeamPointViewSet(viewsets.ModelViewSet):
     queryset = TeamPoint.objects.all()
     serializer_class = TeamPointSerializer
