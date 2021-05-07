3ab2c237c74b7d3ed20bd19dea5f75ee383f1913
joy <joyuuuu1@gmail.com>
2021-05-07 21:40:19 +0900

    [feat#19] 로그인 로그아웃 토큰인증으로 구현

--- checklionproj/checklionproj/urls.py
+++ checklionproj/checklionproj/urls.py
@@ -5,12 +5,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from checklion import urls
 from people import urls
-
+from rest_auth import urls
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
-    #path('api-auth/', include('rest_framework.urls')),
     path('', include('people.urls')),
-    path('check', include('checklion.urls')),
+    path('check/', include('checklion.urls')),
+    path('rest-auth/', include('rest_auth.urls')),
 
 ]
