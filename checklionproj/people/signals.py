75175b509621853eb4704a0c5d4b5c65eca95530
joy <joyuuuu1@gmail.com>
2021-05-10 13:57:11 +0900

    [feat#25] teamscore update, delete시 오류 해결

--- checklionproj/people/signals.py
+++ checklionproj/people/signals.py
@@ -1,10 +1,30 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save,pre_save, post_delete
 from django.dispatch import receiver
 from .models import Team
 from checklion.models import TeamPoint
 
+@receiver(pre_save, sender=TeamPoint)
+def teampoint_pre_save(sender, **kwargs):
+    teampointID = kwargs['instance'].pk
+    team=kwargs['instance'].team
+    try:
+        pre_teampoint = TeamPoint.objects.get(pk=teampointID)
+        pre_weekScore = pre_teampoint.additionalPoint + pre_teampoint.attendancePass + pre_teampoint.lecturePass + pre_teampoint.assignmentPass
+        team.totalScore -= pre_weekScore
+        team.save()
+    except:
+        pass
+
 @receiver(post_save, sender=TeamPoint)
 def teampoint_post_save(sender, **kwargs):
     team=kwargs['instance'].team
-    team.totalScore += kwargs['instance'].weekScore
+    weekScore = kwargs['instance'].additionalPoint + kwargs['instance'].attendancePass + kwargs['instance'].lecturePass + kwargs['instance'].assignmentPass
+    team.totalScore += weekScore
+    team.save()
+
+@receiver(post_delete, sender=TeamPoint)
+def teampoint_post_delete(sender, **kwargs):
+    team=kwargs['instance'].team
+    weekScore = kwargs['instance'].additionalPoint + kwargs['instance'].attendancePass + kwargs['instance'].lecturePass + kwargs['instance'].assignmentPass
+    team.totalScore -= weekScore
     team.save()
